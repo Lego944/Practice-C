3e5b8557e3ad62533ed3c2e30b549abd192612fe
Хабаров Сергей <habarov94@bk.ru>
2022-09-05 06:48:38 +0500

    add Tusk13_HW

--- Seminar2/Tusk13_HW/Program.cs
+++ Seminar2/Tusk13_HW/Program.cs
@@ -4,4 +4,25 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.WriteLine("Программа выводит третью цифру указанного числа");
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
+if(number<0)
+{
+    Console.WriteLine("Число должно быть положительным");
+}
+else if(number > 99)
+{
+    string str = number.ToString();
+    int[] b = new int[str.Length];
+    for( int i=0; i< str.Length; i++)
+    {
+         b[i] = int.Parse(str[i].ToString());
+    }
+    Console.WriteLine($"Третья цифра вашего числа {b[2]}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
