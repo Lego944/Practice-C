9af58ad83c77e97329769de8963b7585b9248738
Хабаров Сергей <habarov94@bk.ru>
2022-09-10 04:26:57 +0500

    add Tusk18

---
+++ Seminar3/Tusk18/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+Console.WriteLine("Введите нормер координатной четверти:");
+int numCor = Convert.ToInt32(Console.ReadLine());
+
+if ( numCor < 5 && numCor > 0)
+{
+    if (numCor == 1)
+    {
+        Console.WriteLine("X > 0 и Y > 0");
+    }
+    if (numCor == 2)
+    {
+        Console.WriteLine("X < 0 и Y > 0");
+    }
+    if (numCor == 3)
+    {
+        Console.WriteLine("X < 0 и Y < 0");
+    }
+    if (numCor == 4)
+    {
+        Console.WriteLine("X > 0 и Y < 0");
+    }
+}
+else
+{ 
+    Console.WriteLine("Введено число больше диапазона четверти или отрицательное");
+}
