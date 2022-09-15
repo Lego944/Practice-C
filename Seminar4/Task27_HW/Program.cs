6fe514f3fe4bec85aac23062ce33510673e5339f
Хабаров Сергей <habarov94@bk.ru>
2022-09-15 19:50:01 +0500

    Add Task27_HW

---
+++ Seminar4/Task27_HW/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumN(int N)
+{
+    int counter = Convert.ToString(N).Length;
+    int A = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        A = N - N % 10;
+        result = result + (N - A);
+        N = N / 10;
+    }
+    return result;
+}
+
+int number = ReadNumber("Введите число, чтобы узнать сумму цифр: ");
+int NumberSum = SumN(number);
+Console.WriteLine("Сумма цифр в числе: " + NumberSum);
